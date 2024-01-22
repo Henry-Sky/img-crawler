7700094d3d3309fab975ba4f461ab02cbeae4019
henry <HenrySky64@outlook.com>
2024-01-22 22:54:13 +0800

    more page crawler

--- bing_crawler.py
+++ bing_crawler.py
@@ -1,30 +1,55 @@
+import random
 import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 import re
 
+# HTML中搜寻目标信息的正则匹配式
 findTitle = re.compile(r'\"t\":(.*?),')
 findNoise = re.compile(r'[, , _, -, |]')
-
 findHref = re.compile(r'href=\"(.*?)\"')
 findMurl = re.compile(r'\"murl\":\"(.*?)\",')
 findLnkw = re.compile(r'<div class=\"lnkw\">(.*?)</div>')
 
 
+def getKeyWord(origin, key, before=2, end=3):
+    keywordlist = re.findall(re.compile(r"\w{0,"+str(before)+"}"+key+"\w{0,"+str(end)+"}"), origin)
+    return keywordlist
+
+
+def getURL(keyword, first, pagenum=35):
+    bin = bytes(keyword, encoding='utf-8').hex()
+    index = 0
+    res = '%'
+    for char in bin:
+        index = index + 1
+        res += char
+        if index % 2 == 0 and index != len(bin):
+            res += '%'
+    return (
+        "https://cn.bing.com/images/search?q=" + res +
+        f"&form=HDRSC2&first={first}&count={pagenum}&relp={pagenum}&scenario=ImageBasicHover&datsrc=N_I&layout=RowBased&mmasync=1"
+    )
+
+def getheader():
+    # 随机请求头
+    headers = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0",
+        "Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)"
+
+    ]
+    index = random.randint(0, int(len(headers) - 1))
+    return {"User-Agent":headers[index]}
+
+
 def requestURL(url):
     # 设置请求标头,伪装成浏览器请求
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
-    }
-    request = urllib.request.Request(url, headers=headers)
-    html = ""
-    try:
-        response = urllib.request.urlopen(request)
-        html = response.read().decode("utf-8")
-    except urllib.error.URLError as e:
-        if hasattr(e, "code"):
-            print(e.code)
-        if hasattr(e, "reason"):
-            print(e.reason)
+    header = getheader()
+    request = urllib.request.Request(url, headers=header)
+    response = urllib.request.urlopen(request)
+    html = response.read().decode("utf-8")
     return html
 
 
@@ -37,32 +62,47 @@ def getData(targethtml):
         item_data = {}
 
         # 网页信息概括
-        head_title = re.findall(findTitle, item)[0]
-        title = re.sub(findNoise, "", head_title)
-        item_data["title"] = title
+        head_title = re.findall(findTitle, item)
+        if len(head_title) != 0:
+            title = re.sub(findNoise, "", head_title[0])
+            item_data["title"] = title
+        else:
+            print("未找到title, 跳过!")
+            continue
 
         # 预览图片链接
-        img_src = re.findall(findMurl, item)
-        img = [x for x in img_src if x != '']
-        if img != []:
+        img_list = re.findall(findMurl, item)
+        img = [x for x in img_list if x != '']
+        if len(img) != 0:
             item_data["img"] = img[0]
         else:
-            item_data["img"] = ""
+            print("未发现图片, 跳过!")
+            continue
 
         # 外部链接
-        lnkw = re.findall(findLnkw, item)[0]
-        href = re.findall(findHref, lnkw)[0]
-        item_data["ref"] = str(href)
+        lnkw_list = re.findall(findLnkw, item)
+        if len(lnkw_list) != 0:
+            href_list = re.findall(findHref, lnkw_list[0])
+            if len(href_list) != 0:
+                item_data["ref"] = str(href_list[0])
+            else:
+                print("未找到href, 跳过!")
+                continue
+        else:
+            print("未找到lnkw, 跳过!")
+            continue
 
         # 打包单个搜索数据
         data.append(item_data)
     return data
 
+
 def saveImg(url, filename):
-    try:
-        urllib.request.urlretrieve(url, filename)
-    except urllib.error as e:
-        print(e.reason)
+    header = getheader()
+    res = urllib.request.Request(url, headers=header)
+    with open(filename, "wb") as f:
+        f.write(urllib.request.urlopen(res).read())
+        print("保存图片: " + filename)
 
 
 """
--- crawler.py
+++ crawler.py
@@ -1,23 +1,33 @@
+import os
+import time
 import bing_crawler as bc
-import re
-
-findKeyWord = re.compile(r'\w{0,2}消防\w{0,3}')
-
-# 关键词: 消防车
-url = "https://cn.bing.com/images/search?q=%E6%B6%88%E9%98%B2%E8%BD%A6&form=HDRSC2&first=1"
-# 关键词: 国外消防车
-url2 = "https://cn.bing.com/images/search?q=%E5%9B%BD%E5%A4%96%E6%B6%88%E9%98%B2%E8%BD%A6&form=HDRSC2&first=1&cw=1513&ch=716"
-# 其他
-url3 = "https://cn.bing.com/images/search?q=%E6%B6%88%E9%98%B2%E8%BD%A6%E5%96%B7%E6%B0%B4&form=HDRSC2&first=1&cw=1513&ch=716"
 
 
-page = bc.requestURL(url3)
-data = bc.getData(page)
+def crawler(keyword):
+    # 起始 i=1 每页35张图片
+    for i in range(1, 1000 ,35):
+        url = bc.getURL(keyword, i)
+        print(f"起始{i}: {url}")
+        page = bc.requestURL(url)
+        data = bc.getData(page)
 
-for item in data:
-    title = item['title']
-    filename = re.findall(findKeyWord, title)[0]
-    img_url = item['img']
-    if img_url != "":
-        bc.saveImg(img_url, "./imgs/" + filename + ".jpg")
-        print(f"{filename}: saved")
+        for item in data:
+            title = item['title']
+            keywordlist = bc.getKeyWord(title, keyword)
+            if len(keywordlist) != 0:
+                filename = keywordlist[0]
+            else:
+                print("无法定位关键词, 跳过")
+                continue
+            img_url = item['img']
+            if os.path.exists(f"imgs/{keyword}"):
+                pass
+            else:
+                os.mkdir(f"imgs/{keyword}/")
+            bc.saveImg(img_url, f"imgs/{keyword}/" + filename + ".jpg")
+            time.sleep(1)
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    crawler("消防车")
